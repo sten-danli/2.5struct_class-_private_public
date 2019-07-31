25f89bb109cfead5ccf45595bc83e06e48e32c7b
Dan Li <akiraryo@hotmail.com>
2019-07-31 17:32:43 +0200

    2- 5　结构、权限修饰符、类简介

---
+++ 2.5project/2.5project/2.5project.cpp
@@ -0,0 +1,16 @@
+//2- 5　结构、权限修饰符、类简介
+#include<iostream>
+using namespace std;
+struct  student
+{
+	//
+	int number;
+	char name[100];
+};
+void func(student tmpstu)
+{
+	tmpstu.number = 2000;
+	strcpy_s(tmpstu.name, sizeof(tmpstu.name), "who");
+	return;
+	
+}
